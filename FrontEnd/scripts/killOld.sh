403858434d8f017f734e3a9bfc51ccef6895c6ae
s3784876 <luke.magnusson999@gmail.com>
2020-07-27 22:55:28 +1000

    Modified search method for old serving scripts

--- FrontEnd/scripts/killOld.sh
+++ FrontEnd/scripts/killOld.sh
@@ -1,3 +1,20 @@
 #kills old node.js instance(s)
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-killp node
+#Sends a kill signal to any stuborn instances
+for INSTANCE in `cat ./toKill.local`;
+do
+  kill $INSTANCE
+  echo $INSTANCE >> toKill.local
+done
+
+echo > toKill.local
+
+#Sends a quit signal to the previous instances
+for INSTANCE in `cat ./pid.local`;
+do
+  kill -3 $INSTANCE
+  echo $INSTANCE >> toKill.local
+done
+
+echo > pid.local
