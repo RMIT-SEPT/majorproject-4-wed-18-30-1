fa7e7f3ff4bc5e33f41017b88e56dfaf262b011f
s3784876 <luke.magnusson999@gmail.com>
2020-07-27 23:16:04 +1000

    chmod to make shell files executable

--- FrontEnd/scripts/launchInBackground.sh
+++ FrontEnd/scripts/launchInBackground.sh
@@ -4,4 +4,4 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #sends messages to both launch.log (instance log) and launch.alog (appending/branch log)
-nohup ./launch.sh | tee ../logs/launch.log >> ../logs/launch.alog 
+nohup ./launch.sh &>> ../logs/launch.alog &
