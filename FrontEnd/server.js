8d992625d61864490b5841bc6ea5f223c8f3c066
s3784876 <luke.magnusson999@gmail.com>
2020-07-27 18:16:40 +1000

    Creating hello world page

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -1,7 +1,14 @@
 var http = require('http');
 var https = require('https')
 var fs = require("fs");
- 
-http.createServer(function(request, response) {}).listen(80);
 
-https.createServer(function(request, response) {}).listen(443)
+
+
+http.createServer(function(request, response) {
+  fs.readFile("index.html", function(err, data){
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(data);
+  response.end();
+});}).listen(80);
+
+//https.createServer(function(request, response) {}).listen(443)
