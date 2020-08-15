4814139258c1ea162a5e70f0553e46cae8cae2ac
Daniel Dominique <s3745409@student.rmit.edu.au>
2020-08-15 15:42:19 +1000

    Added JWT Authentication using spring security

--- FrontEnd/src/App.test.js
+++ FrontEnd/src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import interceptors from "../src/Interceptors";
+import login from "./login";
+import dashboard from "./dashboard";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BrowserRouter>
+          <Route exact path="/" component={login} />
+          <Route exact path="/dashboard" component={dashboard} />
+        </BrowserRouter>
+      </header>
+    </div>
+  );
+}
+
+export default App;
